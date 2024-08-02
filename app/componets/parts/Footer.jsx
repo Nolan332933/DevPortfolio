9f8d724777225caf819ee912ec80700d8fd08242
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-08-03 06:41:03 +0700

    Footer resolved

---
+++ app/componets/parts/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="py-5 bg-slate-700">
+      <div className="flex justify-between mx-5 md:mx-20">
+        <div>
+          <p className="text-slate-400 text-sm">
+            {" "}
+            Copyright © 2024. All Rights Reserved.{" "}
+          </p>
+        </div>
+        <div>
+          <p className="text-slate-400 text-sm"> Developed By Nolan </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
